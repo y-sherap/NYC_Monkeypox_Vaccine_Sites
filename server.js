a555fa26555e1fea25c046695b9c7dabe34d0371
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-07-26 11:14:56 -0400

    added middleware, routes and controllers template

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const routes = require('./routes');
+const db = require('./db');
+const cors = require('cors')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api', routes);
+
+
+
+
+
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
