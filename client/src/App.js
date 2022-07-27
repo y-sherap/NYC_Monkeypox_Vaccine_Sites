e7c7fab623082cf062761db58a845d1bbe40695d
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-07-27 10:50:19 -0400

    testing app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './styles/App.css'
 // import React, { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import ReviewsComponent from './components/ReviewsComponent'
+// import ReviewsComponent from './components/ReviewsComponent'
 import NavBar from './components/NavBar'
 
 
@@ -11,12 +11,13 @@ const App = () => {
   return (
     <div className="App">
       <header>
+        <h1>Hope this works</h1>
        <NavBar />
       </header>
       <main>
         <Routes>
           <Route path="/" element={ <Home /> } />
-          <Route path="reviews" element={ <ReviewsComponent reviews={ reviews } /> } />
+          {/* <Route path="reviews" element={ <ReviewsComponent reviews={ reviews } /> } /> */}
         </Routes>
       </main>
     </div>
