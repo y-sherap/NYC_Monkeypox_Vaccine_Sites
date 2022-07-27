641088f1ddbaaf3c830f2125079e773dea0f1716
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-07-27 14:13:54 -0400

    testing FAQ page

--- client/src/pages/FAQPage.js
+++ client/src/pages/FAQPage.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const FAQPage = () => {
   return (
-    <div>FAQ</div>
+    <div className="mainFAQBody">
+      <div className="FAQTitle">
+        <p>Frequently Asked Questions</p>
+      </div>
+
+      <div>
+        <p>
+          What are the eligibility requirements for the Monkeypox vaccine?
+
+        </p>
+      </div>
+    </div>
+
   )
 }
 
