1e6905f38903c49584ad73e8eeb1e83178263787
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-07-26 23:58:40 -0400

    front end, updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>,
+  document.getElementById('root')
+)
 
