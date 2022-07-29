f0b12015076ffbba2ad6c772e908653305d169aa
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-07-29 10:58:54 -0400

    Heroku main

--- db/index.js
+++ db/index.js
@@ -1,35 +1,17 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/siteDatabase'
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/siteDatabase'
 
 mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-        console.log('Successfully connected to MongoDB.')
-    })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
-
+  .connect(dbUrl)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
-
-
-
-
-
-// const mongoose = require('mongoose')
-
-// mongoose
-//   .connect('mongodb://127.0.0.1:27017/clinicsDatabase') // if we don't have a local database named "productsDatabase" one will be created upon a successful connection
-//   .then(() => {
-//     console.log('Successfully connected to MongoDB.')
-//   })
-//   .catch((e) => {
-//     console.error('Connection error', e.message)
-//   })
-// // mongoose.set('debug', true)
-// const db = mongoose.connection
-
-// module.exports = db
