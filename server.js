b385aaf182690da8b6fb0c00960fa770ab3c650d
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-08-02 13:08:24 -0400

    HDeployment - deleted extranneous files from public

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api', routes);
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(`${__dirname}/client/build/index.html`)
-//  })
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+ })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
