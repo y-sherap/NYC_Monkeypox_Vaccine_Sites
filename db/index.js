1acf8cfe1018811413d0eabfb3d9b836d5c2ac8b
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-08-02 13:12:43 -0400

    replaced mongodb_uri w/ dburl in db/index.js

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ require('dotenv').config() // Add this line
 let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/siteDatabase'
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB!')
   })
